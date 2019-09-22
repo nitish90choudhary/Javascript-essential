959c6c0272d6d227c23e4df580a7518d0df6610c
nitish.choudhary <nitish.choudhary@servicenow.com>
2019-09-22 23:36:03 +0200

    Added String template and Spread operator examples

--- up-and-runing-with-es6/ch02_03/script.js
+++ up-and-runing-with-es6/ch02_03/script.js
@@ -1,7 +1,35 @@
 //Default args in funtions
 
-var nameBuilder = function(firstName="Joe", lastName="Doe") {
-			console.log(firstName + " " + lastName);
-		};
+var nameBuilder = function (firstName = "Joe", lastName = "Doe") {
+	console.log(firstName + " " + lastName);
+};
 
 nameBuilder();
+
+//Template string using backticks ``
+//Example 1
+function print(firstName) {
+	console.log(`Hello ${firstName}`);
+}
+print("Nitish");
+
+//Example 2
+function createEmail(firstName, purchasePrice) {
+	var shipping = 5.95;
+	console.log(`Hi ${firstName}, Thanks for buying from us!!
+	Total: $${purchasePrice}
+	Shipping: $${shipping}
+	Grand Total: $${purchasePrice +shipping}
+	`);
+}
+createEmail("Nitish", 100);
+
+//Spread operators using three dots ...
+console.log("----Spread operators------");
+
+var cats = ["Tabby", "Siamese", "Persian"];
+var dogs = ["Golden Retriever", "Pug", "Schnauzer"];
+
+var animals = ["Whale", "Giraffe", ...cats, "Snake", ...dogs, "Coyote"];
+
+console.log(animals);
