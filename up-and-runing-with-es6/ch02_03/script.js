38102f5e6aaedb3c5e393b58435ab53ed94a6b17
nitish.choudhary <nitish.choudhary@servicenow.com>
2019-09-23 22:16:21 +0200

    Added Enhancement of object literals notations'

--- up-and-runing-with-es6/ch02_03/script.js
+++ up-and-runing-with-es6/ch02_03/script.js
@@ -99,3 +99,36 @@ myactivity();
 
 //with arguments
 myactivity('Nikhil', 'Running', 1);
+
+//Enhancing object literals
+/*
+var cat = {
+	meow: function (times) {
+		console.log(Array(times + 1).join("meow"))
+
+	},
+	purr: function (times) {
+		console.log(Array(times + 1).join("purr"))
+	},
+	snore: function (times) {
+		console.log(Array(times + 1).join("snore"))
+	}
+};
+*/
+//ES6 version of object notation
+
+var cat = {
+	meow(times) {
+		console.log("meow".repeat(times))
+
+	},
+	purr(times) {
+		console.log("purr".repeat(times))
+	},
+	snore(times) {
+		console.log("snore".repeat(times))
+	}
+};
+cat.meow(3);
+cat.purr(4);
+cat.snore(5);
